65b28f36a2392eb0e4ae3e5f546301de8ff1b00c
SarthakBaghel <142529910+SarthakBaghel@users.noreply.github.com>
2024-12-23 03:05:13 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SpecialOffers from './SpecialOffers'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+
+export{
+    PopularProducts,
+    Footer,
+    Hero,
+    CustomerReviews,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality,
+    Services
+}
